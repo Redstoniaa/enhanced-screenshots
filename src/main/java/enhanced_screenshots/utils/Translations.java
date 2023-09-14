4555b3630a4e76db5fb9ba49e00e2fbaa2b51d38
Redstonia <redstonia1234@gmail.com>
2023-09-14 17:47:18 +1200

    Improvements to translations code.

--- src/main/java/enhanced_screenshots/utils/Translations.java
+++ src/main/java/enhanced_screenshots/utils/Translations.java
@@ -8,7 +8,6 @@ import org.apache.commons.io.IOUtils;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -16,69 +15,76 @@ import java.util.Set;
 import static enhanced_screenshots.EnhancedScreenshotsMod.LOGGER;
 
 public class Translations {
-    public static final ClassLoader classLoader = Translations.class.getClassLoader();
-    public static final Gson gson = new Gson();
+    public static final ClassLoader CLASS_LOADER = Translations.class.getClassLoader();
+    public static final Gson GSON = new Gson();
     
-    public static final String languagesPath = "assets/enhanced_screenshots/lang/";
+    public static final String LANGUAGES_PATH = "assets/enhanced_screenshots/lang/";
     
     public static Set<String> supportedLanguages;
     public static final String fallbackLanguage = "en_us";
     public static Map<String, Map<String, String>> translations = new HashMap<>();
     
+    public static String translate(String translationKey, Object... args) {
+        if (isLanguageSupported(getCurrentLanguage()))
+            return translateTo(translationKey, getCurrentLanguage(), args);
+        else if (isLanguageSupported(fallbackLanguage))
+            return translateTo(translationKey, fallbackLanguage, args);
+        return translationKey;
+    }
+    
+    public static String translateTo(String translationKey, String language, Object... args) {
+        return getTranslations(language)
+                .get(translationKey)
+                .formatted(args);
+    }
+    
+    public static Map<String, String> getTranslations(String language) {
+        return translations.get(language);
+    }
+    
+    public static String getCurrentLanguage() {
+        return MinecraftClient.getInstance()
+                .getLanguageManager()
+                .getLanguage();
+    }
+    
+    public static boolean isLanguageSupported(String language) {
+        return supportedLanguages.contains(language);
+    }
+    
     public static void loadTranslations() {
         try {
-            getSupportedLanguages();
+            loadSupportedLanguages();
         } catch (IOException e) {
-            LOGGER.warn("Was unable to read supported languages file.", e);
+            LOGGER.warn("Unable to get supported languages. Falling back on " + fallbackLanguage, e);
             supportedLanguages = Set.of(fallbackLanguage);
         }
         
         for (String languageCode : supportedLanguages) {
             try {
-                Map<String, String> translation = getTranslation(languageCode);
+                Map<String, String> translation = loadTranslation(languageCode);
                 translations.put(languageCode, translation);
             } catch (IOException e) {
-                LOGGER.warn("Was unable to read translations for " + languageCode, e);
+                LOGGER.warn("Unable to read translations for " + languageCode, e);
+                supportedLanguages.remove(languageCode);
             }
         }
     }
     
-    private static void getSupportedLanguages() throws IOException {
-        String content = read(languagesPath + "meta/supported_languages.json");
-        supportedLanguages = gson.fromJson(content, new TypeToken<Set<String>>(){}.getType());
+    private static void loadSupportedLanguages() throws IOException {
+        String content = readResource(LANGUAGES_PATH + "meta/supported_languages.json");
+        supportedLanguages = GSON.fromJson(content, new TypeToken<Set<String>>(){}.getType());
     }
     
-    public static Map<String, String> getTranslation(String languageCode) throws IOException {
-        if (!supportedLanguages.contains(languageCode))
-            return Collections.emptyMap();
-        String content = read(languagesPath + "%s.json".formatted(languageCode));
-        return gson.fromJson(content, new TypeToken<Map<String, String>>(){}.getType());
+    public static Map<String, String> loadTranslation(String languageCode) throws IOException {
+        String content = readResource(LANGUAGES_PATH + "%s.json".formatted(languageCode));
+        return GSON.fromJson(content, new TypeToken<Map<String, String>>(){}.getType());
     }
     
-    private static String read(String resourcePath) throws IOException {
-        InputStream supportedLanguagesStream = classLoader.getResourceAsStream(resourcePath);
+    private static String readResource(String resourcePath) throws IOException {
+        InputStream supportedLanguagesStream = CLASS_LOADER.getResourceAsStream(resourcePath);
         if (supportedLanguagesStream == null)
-            throw new IOException("Fail");
+            throw new IOException("Resource could not be found or accessed.");
         return IOUtils.toString(supportedLanguagesStream, StandardCharsets.UTF_8);
-    }
-    
-    public static String translate(String translationKey, Object... args) {
-        if (isSupported(getCurrentLanguageCode()))
-            return translateFromLanguage(translationKey, getCurrentLanguageCode(), args);
-        else if (isSupported(fallbackLanguage))
-            return translateFromLanguage(translationKey, fallbackLanguage, args);
-        return translationKey;
-    }
-    
-    public static String translateFromLanguage(String translationKey, String languageCode, Object... args) {
-        return translations.get(languageCode).get(translationKey).formatted(args);
-    }
-    
-    public static String getCurrentLanguageCode() {
-        return MinecraftClient.getInstance().getLanguageManager().getLanguage();
-    }
-    
-    public static boolean isSupported(String languageCode) {
-        return supportedLanguages.contains(languageCode);
     }
 }
